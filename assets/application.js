ba79c866692a9a1612a99ab27137fcdbd772f201
松尾敬太 <keita.matsuo@funtip.co.jp>
2021-04-21 10:38:07 +0900

    order-memo

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,10 @@
 // Put your application javascript here
+
+function changeOrderMemo() {
+    if (document.getElementById("order-memo-check").checked) {
+        document.getElementById("order-memo").style.display = "block";
+    } else {
+        document.getElementById("order-memo").style.display = "none";
+    }
+}
+
